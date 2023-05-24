3d34e70d92d74e4bd4fa6dac2ab6fd51f1b95dc7
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2023-05-25 02:20:34 +0300

    move logic from Order controller to model

--- src/mvc/master/controller/Order.controller.ts
+++ src/mvc/master/controller/Order.controller.ts
@@ -2,8 +2,6 @@ import { LayoutType } from "sap/f/library";
 import Dialog from "sap/m/Dialog";
 import ObjectListItem from "sap/m/ObjectListItem";
 import Event from "sap/ui/base/Event";
-import Filter from "sap/ui/model/Filter";
-import FilterOperator from "sap/ui/model/FilterOperator";
 import JSONListBinding from "sap/ui/model/json/JSONListBinding";
 import { Order as OrderType } from "../../../typedef/ODataModelTypes";
 import { OrderDataDialogFragment, OrderView } from "../../../typedef/ViewIds";
@@ -22,60 +20,13 @@ export default class Order extends BaseController<OrderView & OrderDataDialogFra
 		this.getView()?.setModel(this.getModel("OrderModel"));
 
 		this.getRouter().getRoute("Order")?.attachMatched(this._onRouteMatched, this);
-		this._loadOrders();
+		void this.getModel("OrderModel")?.loadOrders();
 
 		sap.ui.getCore().getEventBus().subscribe("OrderApp", "ActionFinished", this._onActionFinished, this);
 	}
 
 	private _onActionFinished(sApp: string, sChannel: string, mData: { OrderID: number }) {
-		this._rereadOrder(mData.OrderID);
-	}
-
-	private _rereadOrder(iOrderId: number) {
-		const oODataModel = this.getModel("ODataModel");
-
-		this.getView()?.setBusy(true);
-		const sPath = oODataModel?.createKey("/Orders", { OrderID: iOrderId });
-		if (!sPath) {
-			return;
-		}
-		oODataModel?.read(sPath, {
-			success: (mRefreshedOrder: OrderType) => {
-				const aOrders = this.getModel("OrderModel")?.getProperty<OrderType[]>("/Orders");
-				const mOldOrder = aOrders?.find(mOrder => mOrder.OrderID === iOrderId);
-				const iIndexOfOldOrder = mOldOrder && aOrders?.indexOf(mOldOrder);
-				if (iIndexOfOldOrder !== undefined) {
-					aOrders?.splice(iIndexOfOldOrder, 1, mRefreshedOrder);
-					this.getModel("OrderModel")?.setProperty("/Orders", aOrders);
-				}
-				this.getView()?.setBusy(false);
-			},
-			error: () => {
-				// do something
-				this.getView()?.setBusy(false);
-			}
-		});
-	}
-
-	private _loadOrders() {
-		const oODataModel = this.getModel("ODataModel");
-
-		this.getView()?.setBusy(true);
-		oODataModel?.read("/Orders", {
-			success: (mResponse: { results: OrderType[] }) => {
-				mResponse.results.forEach(mOrder => {
-					if (typeof mOrder.CreatedAt === "string") {
-						mOrder.CreatedAt = new Date(mOrder.CreatedAt);
-					}
-				});
-				this.getModel("OrderModel")?.setProperty("/Orders", mResponse.results);
-				this.getView()?.setBusy(false);
-			},
-			error: () => {
-				// do something
-				this.getView()?.setBusy(false);
-			}
-		});
+		void this.getModel("OrderModel")?.rereadOrder(mData.OrderID);
 	}
 
 	private _onRouteMatched() {
@@ -105,40 +56,26 @@ export default class Order extends BaseController<OrderView & OrderDataDialogFra
 	protected _onDialogButtonOkPress() {
 		this._oOrderDataDialog?.close();
 
-		const mOrderData: OrderType = {
-			Description: this.byId("idInputDescription").getValue(),
-			CreatedAt: new Date(),
-			Vendor: this.byId("idInputVendor").getValue(),
-			OrderID: 0,
-			State: "1"
-		};
+		void this._applyBusy(async () => {
+			const mOrderData = {
+				Description: this.byId("idInputDescription").getValue(),
+				Vendor: this.byId("idInputVendor").getValue()
+			};
+			await this.getModel("OrderModel")?.createOrder(mOrderData);
+		});
+	}
 
-		this._createOrder(mOrderData);
+	private async _applyBusy<T>(fnAnything: () => Promise<T>) {
+		this.getView()?.setBusy(true);
+		try {
+			return await fnAnything();
+		} finally {
+			this.getView()?.setBusy(false);
+		}
 	}
 
 	protected _onDialogButtonClosePress() {
 		this._oOrderDataDialog?.close();
-	}
-
-	private _createOrder(mOrder: OrderType) {
-		const oODataModel = this.getModel("ODataModel");
-
-		this.getView()?.setBusy(true);
-		oODataModel?.create("/Orders", mOrder as object, {
-			success: (mCreatedOrder: OrderType) => {
-				if (typeof mCreatedOrder.CreatedAt === "string") {
-					mCreatedOrder.CreatedAt = new Date(mCreatedOrder.CreatedAt);
-				}
-				const aOrders = this.getModel("OrderModel")?.getProperty<OrderType[]>("/Orders");
-				aOrders?.push(mCreatedOrder);
-				this.getModel("OrderModel")?.setProperty("/Orders", aOrders);
-				this.getView()?.setBusy(false);
-			},
-			error: () => {
-				// do something
-				this.getView()?.setBusy(false);
-			}
-		});
 	}
 
 	onDatePickerChange() {
@@ -150,48 +87,7 @@ export default class Order extends BaseController<OrderView & OrderDataDialogFra
 	}
 
 	private _filterOrderList() {
-		const oSearchField = this.byId("idSearchField");
-		const oDatePicker = this.byId("idDatePicker");
-
-		const sSearchValue = oSearchField.getValue();
-		const dDate = oDatePicker.getDateValue() as Date;
-
-		const aFilters: Filter[] = [];
-		if (sSearchValue) {
-			aFilters.push(
-				new Filter(
-					[
-						new Filter("OrderID", FilterOperator.EQ, sSearchValue),
-						new Filter({
-							path: "Description",
-							operator: FilterOperator.Contains,
-							value1: sSearchValue,
-							caseSensitive: false
-						})
-					],
-					false
-				)
-			);
-		}
-		if (dDate) {
-			// const dUTCDate = this._toUTC(dDate);
-			const dNextDay = new Date(new Date(dDate.getTime()).setDate(dDate.getDate() + 1));
-			aFilters.push(new Filter("CreatedAt", FilterOperator.BT, this._toUTC(dDate), this._toUTC(dNextDay)));
-		}
-
-		this.byId("idListOrders").getBinding<JSONListBinding>("items")?.filter(aFilters);
-	}
-
-	private _toUTC(dDate: Date) {
-		const iDateUTC = Date.UTC(
-			dDate.getFullYear(),
-			dDate.getMonth(),
-			dDate.getDate(),
-			dDate.getHours(),
-			dDate.getMinutes(),
-			dDate.getSeconds(),
-			dDate.getMilliseconds()
-		);
-		return new Date(iDateUTC);
+		const aOrderListFilters = this.getModel("OrderModel")?.generateOrderListFilters() ?? [];
+		this.byId("idListOrders").getBinding<JSONListBinding>("items")?.filter(aOrderListFilters);
 	}
 }
--- src/mvc/master/model/OrderModel.ts
+++ src/mvc/master/model/OrderModel.ts
@@ -1,6 +1,111 @@
+import Filter from "sap/ui/model/Filter";
+import FilterOperator from "sap/ui/model/FilterOperator";
 import JSONModel from "sap/ui/model/json/JSONModel";
-
+import { Order } from "../../../typedef/ODataModelTypes";
+import ODataModel from "../../base/model/ODataModel";
 /**
  * @namespace com.insidettrack.demo.mvc.master.model
  */
-export default class OrderModel extends JSONModel {}
+export default class OrderModel extends JSONModel {
+	constructor() {
+		super({
+			Orders: [],
+			Filters: {
+				SearchValue: "",
+				Date: null
+			}
+		});
+	}
+
+	async rereadOrder(iOrderId: number) {
+		const oODataModel = ODataModel.getInstance();
+
+		const sPath = oODataModel.createKey("/Orders", { OrderID: iOrderId });
+		const mRefreshedOrder = await ODataModel.getInstance().readAsync<Order>(sPath);
+
+		if (typeof mRefreshedOrder.CreatedAt === "string") {
+			mRefreshedOrder.CreatedAt = new Date(mRefreshedOrder.CreatedAt);
+		}
+		const aOrders = this.getProperty<Order[]>("/Orders");
+		const mOldOrder = aOrders?.find(mOrder => mOrder.OrderID === iOrderId);
+		const iIndexOfOldOrder = mOldOrder && aOrders?.indexOf(mOldOrder);
+		if (iIndexOfOldOrder !== undefined) {
+			aOrders.splice(iIndexOfOldOrder, 1, mRefreshedOrder);
+			this.setProperty("/Orders", aOrders);
+		}
+	}
+
+	async loadOrders() {
+		const oODataModel = ODataModel.getInstance();
+
+		const aOrders = await oODataModel.readAsync<Order[]>("/Orders");
+		aOrders.forEach(mOrder => {
+			if (typeof mOrder.CreatedAt === "string") {
+				mOrder.CreatedAt = new Date(mOrder.CreatedAt);
+			}
+		});
+
+		this.setProperty("/Orders", aOrders);
+	}
+
+	async createOrder(mOrderData: Pick<Order, "Description" | "Vendor">) {
+		const oODataModel = ODataModel.getInstance();
+
+		const mOrder: Order = {
+			...mOrderData,
+			CreatedAt: new Date(),
+			OrderID: 0,
+			State: "1"
+		};
+		const mCreatedOrder = await oODataModel.createAsync<Order>("/Orders", mOrder);
+
+		if (typeof mCreatedOrder.CreatedAt === "string") {
+			mCreatedOrder.CreatedAt = new Date(mCreatedOrder.CreatedAt);
+		}
+		const aOrders = this.getProperty<Order[]>("/Orders");
+		aOrders?.push(mCreatedOrder);
+		this.setProperty("/Orders", aOrders);
+	}
+
+	generateOrderListFilters() {
+		const aFilters: Filter[] = [];
+		const sSearchValue = this.getProperty<string>("/Filters/SearchValue");
+		const dDate = this.getProperty<Date>("/Filters/Date");
+		if (sSearchValue) {
+			aFilters.push(
+				new Filter(
+					[
+						new Filter("OrderID", FilterOperator.EQ, sSearchValue),
+						new Filter({
+							path: "Description",
+							operator: FilterOperator.Contains,
+							value1: sSearchValue,
+							caseSensitive: false
+						})
+					],
+					false
+				)
+			);
+		}
+		if (dDate) {
+			// const dUTCDate = this._toUTC(dDate);
+			const dNextDay = new Date(new Date(dDate.getTime()).setDate(dDate.getDate() + 1));
+			aFilters.push(new Filter("CreatedAt", FilterOperator.BT, this._toUTC(dDate), this._toUTC(dNextDay)));
+		}
+
+		return aFilters;
+	}
+
+	private _toUTC(dDate: Date) {
+		const iDateUTC = Date.UTC(
+			dDate.getFullYear(),
+			dDate.getMonth(),
+			dDate.getDate(),
+			dDate.getHours(),
+			dDate.getMinutes(),
+			dDate.getSeconds(),
+			dDate.getMilliseconds()
+		);
+		return new Date(iDateUTC);
+	}
+}
